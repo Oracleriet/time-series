cbdeab89a6f7704f7827fcbbc020bf1ab3973793
Oracleriet <lars@oracleriet.com>
2024-06-18 10:05:09 +0000

    Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP Dashboard - Customized',
+    page_title='GDP DashTboard - Customized',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
